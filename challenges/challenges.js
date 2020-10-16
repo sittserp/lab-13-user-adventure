562aa405ea0fd92ee68f9d98787271904c72ddc6
Perry Sittser <sittserp@gmail.com>
2020-10-16 09:55:40 -0700

    display choices

--- challenges/challenges.js
+++ challenges/challenges.js
@@ -11,12 +11,43 @@ const challenge = findById(challenges, id);
 
 const h1 = document.createElement('h1');
 const h3 = document.createElement('h3');
+const form = document.createElement('form');
 
 h1.textContent = challenge.title;
 h3.textContent = challenge.description;
 
-
 section.appendChild(h1);
 section.appendChild(h3);
+section.appendChild(form);
+
+challenge.choices.forEach(choice => {
+    const label = document.createElement('label');
+    const span = document.createElement('span');
+    const radio = document.createElement('input');
+    radio.type = 'radio';
+    radio.name = 'choice';
+    radio.value = choice.id;
+
+    span.textContent = choice.description;
+
+    label.append(radio, span);
+
+    form.appendChild(label);
+
+
+});
+
+const button = document.createElement('button');
+button.type = 'submit';
+button.textContent = 'Confirm';
+
+form.appendChild(button);
+
+// get form data and hp/supplies from array like initial signup page 
+// and add to local storage
+button.addEventListener('click' => {
+
+})
+
 
 console.log(challenge.choices);
